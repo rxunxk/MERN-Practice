a0af03b1c51b97349a11033f5b3faa7d32b121e0
Raunak <pandeyraunak64@gmai.com>
2023-08-12 22:50:20 +0530

    +mongo db compass setup

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const server = express();
 const productRouter = require("./routes/productRouter");
 const userRouter = require("./routes/userRouter");
 
+console.log("env: ", process.env.DB_PASSWORD);
+
 server.use(express.json()); //bodyParser
 server.use(morgan("default"));
-server.use(express.static("public"));
+server.use(express.static(process.env.PUBLIC));
+
 server.use("/products", productRouter.routes); // now the base url has been changed to /api and all the endpoints are going to work with api/xyz
 server.use("/users", userRouter.routes);
 
-server.listen(8080, (req, res) => {
+server.listen(process.env.PORT, (req, res) => {
   console.log("server started!");
 });
